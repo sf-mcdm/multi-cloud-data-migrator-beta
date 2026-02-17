a03f89bd869d6d428013dbf9d86848075124789a
sf-mcdm <loudrassiahmed>
2026-02-18 00:13:41 +0100

    release: v1.258.155

---
+++ dist/cloudsconfig/digital-insurance-product-catalog.d.ts
@@ -0,0 +1,4 @@
+import type { ObjectCfg } from "../lib/types.js";
+export declare const relatedProductQueries: string[];
+declare const productSliceConfig: ObjectCfg[];
+export default productSliceConfig;

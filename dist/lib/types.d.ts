99d749406ccff25c7baa73b01e8b6013e6d8c7b9
sf-mcdm <loudrassiahmed>
2026-03-25 19:29:45 +0100

    release: v1.258.167

---
+++ dist/lib/types.d.ts
@@ -0,0 +1,42 @@
+export interface CompositeKeyCfg {
+    required: string[];
+    optional?: string[];
+}
+export interface PolymorphicTargetCfg {
+    sobject: string;
+    externalIdField?: string;
+    compositeKey?: CompositeKeyCfg | string[];
+    lookups?: LookupCfg[];
+}
+export interface LookupCfg {
+    field: string;
+    sobject?: string;
+    externalId?: string;
+    targetField: string;
+    targetSobjectCompositeKey?: CompositeKeyCfg | string[];
+    polymorphicConfig?: Record<string, PolymorphicTargetCfg>;
+}
+export interface ObjectCfg {
+    scope?: "migrationOnly" | "destructionOnly" | "both";
+    sobject: string;
+    query: string;
+    externalIdField?: string;
+    compositeKey?: CompositeKeyCfg | string[];
+    lookups: LookupCfg[];
+    overrides?: Record<string, any>;
+    overridesForDelete?: Record<string, any>;
+    deleteAfterUpdate?: boolean;
+    useRestApi?: boolean;
+    binaryFields?: string[];
+    mode?: "insert-only";
+    preQueries?: Array<{
+        name: string;
+        query: string;
+    }>;
+    fieldsToExclude?: string[];
+}
+export interface PcmConfigModule {
+    default: ObjectCfg[];
+    preSKUQueries?: string[];
+    relatedProductQueries?: string[];
+}
